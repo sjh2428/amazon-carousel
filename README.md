52cd7d77c10a6503013c50b5916cdfdc911de851
sjh2428 <pok_gare@naver.com>
2019-09-16 19:39:35 +0900

    feat : add project skeleton #1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 멤버십 프로젝트 -  아마존 저장소
+
+### 3주차 amazon-carousel
+#### 프론트 & 백엔드 개발환경 구축
+- Express 서버 구축
+    - View Engine : Pug
+
+- Webpack
+    - babel-loader
+    - sass-loader
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+
+module.exports = {
+    // entry file
+    entry: "./src/js/main.js",
+    // compile + bundling된 js파일이 저장될 경로와 이름 지정
+    output: {
+        path: path.resolve(__dirname, "public/javascripts"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                // test는 일반적으로 파일 확장명을 일치시키는데 사용
+                test: /\.js$/,
+                // include는 일반적으로 디렉토리를 일치시키는데 사용
+                include: [
+                    path.resolve(__dirname, "src/js")
+                ],
+                // 제외할 파일을 일치시키는데 사용
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }, {
+                test: /\.s[ac]ss$/,
+                include: [
+                    path.resolve(__dirname, "src/sass")
+                ],
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    devtool: "source-map",
+    // https://webpack.js.org/configuration/mode/
+    mode: "development"
+};
