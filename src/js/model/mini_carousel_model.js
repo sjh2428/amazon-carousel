d13a61ee10215eb46fc8f2533e65187b2d2c4340
sjh2428 <pok_gare@naver.com>
2019-09-18 13:28:05 +0900

    refactor : modulize mini carousel data
    modulize mini carousel data by dividing to miniCarouselData.js file.

--- src/js/model/mini_carousel_model.js
+++ src/js/model/mini_carousel_model.js
@@ -1,4 +1,5 @@
 import config from "../config";
+import miniCarouselData from "../miniCarouselData";
 
 class MiniCarouselModel {
     constructor() {
@@ -6,12 +7,18 @@ class MiniCarouselModel {
             <div class="${config.classNames.miniCarouselWrapperClassName}">
                 <div class="${config.classNames.miniCarouselLeftBtnClassName} ${config.classNames.miniCarouselMoveBtnClassName}"></div>
                 <div class="${config.classNames.miniCarouselViewportClassName}">
-                    <ul class="${config.classNames.miniCarouselUlClassName}">
-                        <li class="${config.classNames.miniCarouselLiClassName}" posidx=1>1번마</li>
-                        <li class="${config.classNames.miniCarouselLiClassName}" posidx=2>2번마</li>
-                        <li class="${config.classNames.miniCarouselLiClassName}" posidx=3>3번마</li>
-                        <li class="${config.classNames.miniCarouselLiClassName}" posidx=4>4번마</li>
-                    </ul>
+                    <ul class="${config.classNames.miniCarouselUlClassName}">`;
+        miniCarouselData.forEach((data, idx) => {
+            this.miniCarouselHTML += `
+                <li class="${config.classNames.miniCarouselLiClassName}" posidx=${idx + 1}>
+                    <a href="${data.link}">
+                        <img src="${data.imgSrc}" 
+                        alt="${data.description}" target="_blank">
+                    </a>
+                </li>
+            `;
+        });
+        this.miniCarouselHTML += `</ul>
                 </div>
                 <div class="${config.classNames.miniCarouselRightBtnClassName} ${config.classNames.miniCarouselMoveBtnClassName}"></div>
             </div>
