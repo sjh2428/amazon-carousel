9f849a3c53d4b5ae7b176247b4f111b056d203e4
sjh2428 <pok_gare@naver.com>
2019-09-24 23:09:18 +0900

    feat: add environment for redis session

--- app.js
+++ app.js
@@ -1,41 +1,64 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const redis = require("redis");
+const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
+const dotenv = require("dotenv");
 
-const indexRouter = require('./routes/index');
-const apiRouter = require('./routes/api_router');
+const redisClient = redis.createClient();
+const indexRouter = require("./routes/index");
+const apiRouter = require("./routes/api_router");
 
 const app = express();
 
+dotenv.config();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(session({
+    store: new RedisStore({
+        client: redisClient,
+        host: process.env.REMOTE_HOST,
+        port: process.env.REDIS_PORT
+    }),
+    secret: "secret",
+    resave: true, // 요청 때 세션이 수정된게 없으면 강제로 세션 저장
+    saveUninitialized: true, // 저장된게 없으면 세션 저장
+    cookie: {
+        path: "/",
+        httpOnly: true,
+        secure: false,
+        maxAge: 1000 * 60
+    }
+}));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api', apiRouter);
+app.use("/", indexRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
