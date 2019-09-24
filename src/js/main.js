ae12e4e3858039ce93a0dfc282b6da7aca8d8ec4
sjh2428 <pok_gare@naver.com>
2019-09-24 19:04:14 +0900

    feat: set environment using semantic

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,24 @@
 import "../sass/style.scss"
 
-import CarouselView from "./view/carousel_view";
+// import CarouselView from "./view/carousel_view";
 import CardCarouselView from "./view/main_carousel_view";
 import CarouselModel from "./model/carousel_model";
-import CarouselController from "./controller/carousel_controller";
+// import CarouselController from "./controller/carousel_controller";
 import CardCarouselController from "./controller/main_carousel_controller";
 
-import miniCarouselData from "./model/data/mini_carousel";
+// import miniCarouselData from "./model/data/mini_carousel";
 import mainCarouselData from "./model/data/main_carousel";
 
-import miniCarouselConfig from "./config/mini_carousel_config";
+// import miniCarouselConfig from "./config/mini_carousel_config";
 import mainCarouselConfig from "./config/main_carousel_config";
 
-const MiniCarouselInit = () => {
-    const Model = new CarouselModel(miniCarouselData);
-    const View = new CarouselView(Model, miniCarouselConfig);
-    const Controller = new CarouselController(View, miniCarouselConfig);
-};
+// const MiniCarouselInit = () => {
+//     const Model = new CarouselModel(miniCarouselData);
+//     const View = new CarouselView(Model, miniCarouselConfig);
+//     const Controller = new CarouselController(View, miniCarouselConfig);
+// };
 
-MiniCarouselInit();
+// MiniCarouselInit();
 
 const mainCarouselInit = () => {
     const Model = new CarouselModel(mainCarouselData);
