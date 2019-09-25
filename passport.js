fcfd0745ec5939e540caf3cf8a1c216b554977a6
sjh2428 <pok_gare@naver.com>
2019-09-25 13:31:09 +0900

    fix: serializer, deserializer data change

--- passport.js
+++ passport.js
@@ -3,12 +3,11 @@ const sql = require("./models/sqlQuery");
 
 const passportSetting = (passport) => {
     passport.serializeUser((user, done) => {
-        done(null, user.user_id);
+        done(null, user);
     });
     
-    passport.deserializeUser(async(user_id, done) => {
-        const sqlResult = await sql(`select * from user where user_id='${user_id}'`);
-        done(null, sqlResult[0]);
+    passport.deserializeUser(async(user, done) => {
+        done(null, user);
     });
     
     passport.use(new LocalStorage({
@@ -17,11 +16,8 @@ const passportSetting = (passport) => {
         },
         async(username, password, done) => {
             const sqlResult = await sql(`select * from user where user_id='${username}' and user_password='${password}'`);
-            if (sqlResult.length > 0) {
-                return done(null, sqlResult[0]);
-            } else {
-                return done(null, false, { message: "incorrect id or password" });
-            }
+            if (sqlResult.length > 0) return done(null, sqlResult[0]);
+            else return done(null, false, { message: "incorrect id or password" });
         }
     ));
 }
