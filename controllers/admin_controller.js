7b90b4f5804147eabe2a9cc28bb0c1144bb0ae8d
sjh2428 <pok_gare@naver.com>
2019-09-25 17:03:58 +0900

    feat: add admin user router and controller #8

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -1,3 +1,8 @@
+const sqlQuery = require("../models/sql_query");
+
 module.exports = {
-    
+    async getUsers(user_id) {
+        const users = await sqlQuery(`select * from user where user_id not in ('admin', '${user_id}')`);
+        return users;
+    }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const auth = require("../auth");
-const sqlQuery = require("../models/sql_query");
+const adminController = require("../controllers/admin_controller");
 
 router.get("/", auth.onlyAdmin, (req, res) => {
     res.render("admin/admin", { user: req.user });
 });
 
-router.get("/users", auth.onlyAdmin, (req, res) => {
-
+router.get("/users", auth.onlyAdmin, async(req, res) => {
+    const userData = await adminController.getUsers(req.user.user_id);
+    for (user of userData) user.birth = new Date(user.birth).toLocaleDateString("ko-KR");
+    res.render("admin/users", { user: req.user, userData});
 });
 
 router.get("/item", auth.onlyAdmin, (req, res) => {
