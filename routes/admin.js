a451e1143528f14e57025e4c9b8f400b08004c41
sjh2428 <pok_gare@naver.com>
2019-09-25 17:48:39 +0900

    style: change to destructure import in admin router

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const auth = require("../auth");
+const { onlyAdmin } = require("../auth");
 const adminController = require("../controllers/admin_controller");
 
-router.get("/", auth.onlyAdmin, (req, res) => {
+router.get("/", onlyAdmin, (req, res) => {
     res.render("admin/admin", { user: req.user });
 });
 
-router.get("/users", auth.onlyAdmin, async(req, res) => {
+router.get("/users", onlyAdmin, async(req, res) => {
     const userData = await adminController.getUsers(req.user.user_id);
     for (user of userData) user.birth = new Date(user.birth).toLocaleDateString("ko-KR");
     res.render("admin/users", { user: req.user, userData});
 });
 
-router.get("/dealUser/:id", auth.onlyAdmin, async(req, res) => {
+router.get("/dealUser/:id", onlyAdmin, async(req, res) => {
     const { params: { id }, query: { to } } = req;
     await adminController.authControl(id, to);
     res.redirect("/admin/users");
 });
 
-router.get("/item/upload", auth.onlyAdmin, (req, res) => {
+router.get("/item/upload", onlyAdmin, (req, res) => {
 
 });
 
-router.post("/item/upload", auth.onlyAdmin, (req, res) => {
+router.post("/item/upload", onlyAdmin, (req, res) => {
 
 });
 
